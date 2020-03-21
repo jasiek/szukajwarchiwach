4a154cf2114632a99b5bdd88516a3a85c2e9d5e9
Jan Szumiec <jan.szumiec@gmail.com>
2020-03-21 15:17:50 +0100

    Skrypt działa.

--- get_picture_urls_for_set.py
+++ get_picture_urls_for_set.py
@@ -4,8 +4,11 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-jednostka_re = re.compile("^https://www.szukajwarchiwach.gov.pl/jednostka/-/jednostka/\d+")
+jednostka_re = re.compile(
+    "^https://www.szukajwarchiwach.gov.pl/jednostka/-/jednostka/\d+"
+)
 site_re = re.compile("^https://www.szukajwarchiwach.gov.pl/")
+
 
 class Depaginator:
     def __init__(self, session):
@@ -13,16 +16,14 @@ class Depaginator:
 
     def _next_page_url(self, url):
         page = self.s.get(url)
-        with open("url", "w") as f:
-            f.write(page.text)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, "html.parser")
         widget = soup.select("ul.pagination li a span.icon-caret-right")
         if widget != []:
             a = widget.parent
             href = a.get("href")
             if site_re.match(href):
                 return href
-        
+
     def paginate(self, url):
         yield url
         while True:
@@ -31,28 +32,38 @@ class Depaginator:
                 yield url
             else:
                 break
-        
+
     def crawl(self, url, extractor):
         for u in self.paginate(url):
             for x in extractor(u, self.s):
                 yield x
 
+
 def jednostka_url_extractor(url, session):
     page = session.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    elements = soup.select('td.col-md-5 a')
+    soup = BeautifulSoup(page.content, "html.parser")
+    elements = soup.select("td.col-md-5 a")
     for a in elements:
         href = a.get("href")
         if href and jednostka_re.match(href):
             yield href
 
+# Wyciąga identyfikator zdjęcia, które można pociągnąć przez "API"
 def picture_id_extractor(url, session):
     page = session.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    elements = soup.select('div.jednostka-skan a.load-photo-slider')
+    soup = BeautifulSoup(page.content, "html.parser")
+    elements = soup.select("div.jednostka-skan a.load-photo-slider")
     for e in elements:
         id = e.get("data-plikid")
         yield id
+
+# Wyciąga link ze strony zespołu danych.
+def jednostki_url_extractor(url, session):
+    page = session.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    element = soup.select("a#przejdzDoJednostek")[0]
+    return element.get("href")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -62,10 +73,14 @@ if __name__ == "__main__":
     zespol_id = int(sys.argv[1])
     session = requests.Session()
     zbiory_crawler = Depaginator(session)
-    for u in zbiory_crawler.crawl("https://www.szukajwarchiwach.gov.pl/zespol?p_p_id=Zespol&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_Zespol_javax.portlet.action=zmienWidok&_Zespol_nameofjsp=jednostki&_Zespol_id_zespolu={zespol_id}", jednostka_url_extractor):
+    for u in zbiory_crawler.crawl(
+        jednostki_url_extractor(
+            f"https://www.szukajwarchiwach.gov.pl/zespol/-/zespol/{zespol_id}", session
+        ),
+        jednostka_url_extractor,
+    ):
         jednostki_crawler = Depaginator(session)
         for z in jednostki_crawler.crawl(u, picture_id_extractor):
-            print("https://www.szukajwarchiwach.gov.pl/o/pliki-api/pliki/pobierzplikjpeg/{z}")
-
-    
-    
+            print(
+                f"https://www.szukajwarchiwach.gov.pl/o/pliki-api/pliki/pobierzplikjpeg/{z}"
+            )
